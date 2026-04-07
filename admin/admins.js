ece1f01dd8f2551f209a11ea5bd5a2a0d3522dab
Elauullido <elauullido@gmail.com>
2026-04-07 00:39:47 -0400

    Panel Admin 3v

---
+++ admin/admins.js
@@ -0,0 +1,9 @@
+// BASE DE DATOS DE ADMINISTRADORES DE RYO STUDIOS
+const listaAdmins = {
+    "Lucas": "Ryo2026!",      // Admin: Lucas | Key: Ryo2026!
+    "Staff1": "Minecraft123", // Admin: Staff1 | Key: Minecraft123
+    "Editor": "RyoVip77"      // Puedes añadir todos los que quieras aquí
+};
+
+// Exportar para que el index lo pueda leer
+window.listaAdmins = listaAdmins;
